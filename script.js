726b0d4fa84f9d23f238e10fdcd8a528360422a7
kungotisknut <91283221+kungotisknut@users.noreply.github.com>
2025-09-17 19:56:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    const addEventBtn = document.getElementById("addEventBtn");
+    const dayDivs = document.querySelectorAll(".days");
+
+    const savedEvents = JSON.parse(localStorage.getItem("events")) || [];
+    savedEvents.forEach(event => {
+        renderEvent(event.day, event.time, event.subject, event.color);
+    });
+
+    addEventBtn.addEventListener("click", () => {
+        const day = prompt("enter day").toLowerCase();
+        const time = prompt("enter time");
+        const subject = prompt("enter subject");
+        const color = prompt("enter color (use no space forcolors like light blue)");
+
+        if(!day || !time || !subject){
+            alert("please fill in all fields");
+            return;
+        }
+
+        const newEvent = {day, time, subject, color};
+        savedEvents.push(newEvent);
+        localStorage.setItem("events", JSON.stringify(savedEvents));
+
+        renderEvent(day, time, subject, color);
+    });
+
+    function renderEvent(day, time, subject, color){
+            dayDivs.forEach(div => {
+                if (div.querySelector("h2").textContent.toLowerCase() === day) {
+                    const newEvent = document.createElement("div");
+                    newEvent.classList.add("event");
+                    newEvent.textContent = `${time} ${subject}`;
+                    newEvent.style.backgroundColor = color;
+                    newEvent.style.fontFamily = "sans-serif"
+                    div.appendChild(newEvent);
+                }
+            });
+    }
+});
